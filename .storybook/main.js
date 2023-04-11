e72c53fca17e0b873e2390bf67840e647dde6cc3
Tristan Herwood <tph74@cornell.edu>
2023-04-11 12:23:25 -0400

    build

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ["../public", "../src/assets"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/vue3",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+};
